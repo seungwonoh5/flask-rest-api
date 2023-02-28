f311827edd1346303057c1cf7a145b425470efad
seungwonoh5 <soh1@terpmail.umd.edu>
2023-02-27 21:49:31 -0500

    Created items in stores REST API using Flask-Smorest, marshmallow with latest Dockerfile

---
+++ db.py
@@ -0,0 +1,12 @@
+stores = {
+        "1": {
+         "name": "My Store",   
+         "price": 17.99
+        },
+        "2": {
+            "name": "My Store2",
+            "price": 1.99
+        }
+}
+
+items = {}
